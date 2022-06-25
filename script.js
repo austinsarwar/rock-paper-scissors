9e3f6f83a32dd79e63d561a104c88f912c470024
Austin Sarwar <austin_sarwar@yahoo.com>
2022-06-24 21:23:56 -0400

    Create game logic

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
 let playerScore = 0;
 let computerScore = 0;
-let playerInput = 0;
-let computerInput = Math.floor(Math.random() * 3 )+1;
+let playerInput = "rock";
+let computerInput;
+let randomNumber = Math.floor(Math.random() * 3 )+1;
+
+function computerChoice(computerChoice){
+    
+    if(computerChoice == 1){
+            
+       computerInput = "rock";
+    }else if (computerChoice == 2){
+        computerInput = "paper"
+    }else if (computerChoice = 3) {
+        computerInput = "scissors"
+    }
+    return computerInput
+}
+computerChoice(randomNumber);
 
 
-if(playerInput == "rock".toLowerCase && computerInput == 2){
+if(playerInput == "rock" && computerInput == "paper"){
     console.log("Computer wins");
     computerScore ++;
-}else if(playerInput == "rock".toLowerCase && computerInput == 3){
+}else if(playerInput == "rock" && computerInput == "scissors"){
     playerScore++;
     console.log("Player wins");
     
-}else if(playerInput == "paper".toLowerCase && computerInput == 1){
+}else if(playerInput == "paper" && computerInput == "rock"){
     playerScore++;
     console.log("Player wins");
 }
-else if(playerInput == "paper".toLowerCase && computerInput == 3){
+else if(playerInput == "paper" && computerInput == "scissors"){
     computerScore++;
     console.log("Computer wins");
-}else if (playerInput == "scissors".toLowerCase && computerInput == 1){
+}else if (playerInput == "scissors" && computerInput == "rock"){
     computerScore++;
     console.log("Computer wins");
-}else if (playerInput == "scissors".toLowerCase && computerInput == 2){
+}else if (playerInput == "scissors" && computerInput == "paper"){
     playerScore++;
     console.log("Player wins");
-}else{
-  console.log("Tie game");
 }
-console.log("Player Score:" + playerScore + "Computer Score:" + computerScore);
-console.log("Computer played:" + computerInput);
+
+console.log("Player PLays: " + playerInput + "/" + " Computer Plays: " + computerInput);
